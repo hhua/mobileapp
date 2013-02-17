f1100caa2ebb6c506bd243fd83b37727c1566e1c
Jon <jetan2@gmail.com>
2013-02-16 20:36:37 -0500

    basic relationships

--- eventrecommender/app/models/attendee.rb
+++ eventrecommender/app/models/attendee.rb
@@ -1,3 +1,6 @@
 class Attendee < ActiveRecord::Base
   attr_accessible :attending, :event_id, :rating, :saved, :user_id
+
+  belongs_to :user
+  belongs_to :event
 end
--- eventrecommender/app/models/event.rb
+++ eventrecommender/app/models/event.rb
@@ -1,3 +1,6 @@
 class Event < ActiveRecord::Base
   attr_accessible :date, :location, :name, :url
+
+  has_many :attendees
+  has_many :event_interests
 end
